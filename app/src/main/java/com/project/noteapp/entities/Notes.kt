ace0abe5aed121155288770a4f4f934225ed5a94
infam0us94 <ivanpopoff94@mail.ru>
2021-06-08 15:40:17 +0300

    room

---
+++ app/src/main/java/com/project/noteapp/entities/Notes.kt
@@ -0,0 +1,32 @@
+package com.project.noteapp.entities
+
+import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+@Entity(tableName = "Notes")
+data class Notes(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int,
+    @ColumnInfo(name = "title")
+    var title: String,
+    @ColumnInfo(name = "sub_title")
+    var subTitle: String,
+    @ColumnInfo(name = "date_time")
+    var dateTime: String,
+    @ColumnInfo(name = "note_text")
+    var noteText: String,
+    @ColumnInfo(name = "img_path")
+    var imgPath: String,
+    @ColumnInfo(name = "web_link")
+    var webLink: String,
+    @ColumnInfo(name = "color")
+    var color: String
+) : Parcelable {
+    override fun toString(): String {
+        return "$title : $dateTime"
+    }
+}
